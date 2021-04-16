a5aa4f37c69f76ba197701bd9be1c731e23434c2
MISTCLICK <yakchanell@gmail.com>
2021-04-16 23:03:02 +0300

    info Command added

---
+++ cmds/misc/info.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+const discord_js_commando_1 = require("discord.js-commando");
+const config_json_1 = require("../../config.json");
+class MetarCommand extends discord_js_commando_1.Command {
+    constructor(client) {
+        super(client, {
+            name: 'info',
+            description: 'Information about the bot!',
+            memberName: 'info',
+            group: 'misc',
+            aliases: ['i'],
+        });
+    }
+    async run(message) {
+        const embed = new discord_js_1.MessageEmbed()
+            .setColor(config_json_1.mainColor)
+            .setFooter(config_json_1.mainFooter)
+            .setDescription("Version 1.0\n\n **What is my purpose?**\n\n The purpose of this bot is to provide useful information to this server's members and help staff manage it. Use !help to find out what I can do.\n\n **About the project.**\n\n This bot is custom-made by <@349553169035952140>\n The bot is written in JavaScript.\n\n **Bug to report?**\n\n Report any bug by messaging the staff.\n\n **Author**\n\n <@349553169035952140>\n *Currently running in a stable server environment.*")
+            .setAuthor('Information about this bot', this.client.user?.displayAvatarURL(), 'https://github.com/MISTCLICK/');
+        return message.reply(embed);
+    }
+}
+exports.default = MetarCommand;
---
+++ cmds/misc/info.ts
@@ -0,0 +1,26 @@
+import { MessageEmbed } from 'discord.js';
+import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
+import { mainColor, mainFooter } from '../../config.json';
+
+export default class MetarCommand extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: 'info',
+      description: 'Information about the bot!',
+      memberName: 'info',
+      group: 'misc',
+      aliases: ['i'],
+
+    });
+  }
+
+  async run(message: CommandoMessage,) {
+    const embed = new MessageEmbed()
+      .setColor(mainColor)
+      .setFooter(mainFooter)
+      .setDescription("Version 1.0\n\n **What is my purpose?**\n\n The purpose of this bot is to provide useful information to this server's members and help staff manage it. Use !help to find out what I can do.\n\n **About the project.**\n\n This bot is custom-made by <@349553169035952140>\n The bot is written in JavaScript.\n\n **Bug to report?**\n\n Report any bug by messaging the staff.\n\n **Author**\n\n <@349553169035952140>\n *Currently running in a stable server environment.*")
+      .setAuthor('Information about this bot', this.client.user?.displayAvatarURL(), 'https://github.com/MISTCLICK/')
+
+    return message.reply(embed);
+  }
+}
